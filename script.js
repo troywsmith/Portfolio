41bcf4bb5699963f157e423dd86eef481a054745
Troy <36167978+troywsmith@users.noreply.github.com>
2018-05-06 21:43:06 -0400

    fixed js file

--- script.js
+++ script.js
@@ -1,81 +1,72 @@
-body {
-    font-family: 'Pinyon Script', cursive;
-    background-color: black;
-    color: white;
-    text-align: center;
-}
+$(document).ready(function () {
+    function game() {
+        var turnCount = 0;
+        var grid = [null, null, null, null, null, null, null, null, null];
+        $('#board tr td').click(function () {
+            if ((turnCount % 2 === 0)) {
+                $(this).append("X");
+                $(this).css('background-color', 'gray');
+                $('div #playerturn').html("Player 2 (O) - Please choose a box");
+                turnCount++;
+                var grid = $('#board tr td').map(function () {
+                    return $(this).text();
+                }).get();
+                checkForWin();
+                console.log(grid);
+                return grid;
+            } else {
+                $(this).append("O");
+                $(this).css('background-color', 'gray');
+                $('div #playerturn').html("Player 1 (X) - Please choose a box");
+                turnCount++;
+                var grid = $('#board tr td').map(function () {
+                    return $(this).text();
+                }).get();
+                checkForWin();
+                console.log(grid);
+                return grid;
+            }
+            function checkForWin() {
+                if (
+                    //grid row winners
+                    (grid[0] === 'X' && grid[1] === "X" && grid[2] === "X")
+                    || (grid[3] === 'X' && grid[4] === "X" && grid[5] === "X")
+                    || (grid[6] === 'X' && grid[7] === "X" && grid[8] === "X")
+                    //grid column winners 
+                    || (grid[0] === 'X' && grid[3] === "X" && grid[6] === "X")
+                    || (grid[1] === 'X' && grid[4] === "X" && grid[7] === "X")
+                    || (grid[2] === 'X' && grid[5] === "X" && grid[8] === "X")
+                    //grid diag winners
+                    || (grid[0] === 'X' && grid[4] === "X" && grid[8] === "X")
+                    || (grid[2] === 'X' && grid[4] === "X" && grid[6] === "X")) {
+                    alert("X has won!! Game Over");
+                } else if (
+                    //grid row winners
+                    (grid[0] === 'X' && grid[1] === "X" && grid[2] === "X")
+                    || (grid[3] === 'O' && grid[4] === "O" && grid[5] === "O")
+                    || (grid[6] === 'O' && grid[7] === "O" && grid[8] === "O")
+                    //grid column winners
+                    || (grid[0] === 'O' && grid[3] === "O" && grid[6] === "O")
+                    || (grid[1] === 'O' && grid[4] === "O" && grid[7] === "O")
+                    || (grid[2] === 'O' && grid[5] === "O" && grid[8] === "O")
+                    //grid diag winners
+                    || (grid[0] === 'O' && grid[4] === "O" && grid[8] === "O")
+                    || (grid[1] === 'O' && grid[4] === "O" && grid[6] === "O")) {
+                    alert("O has won!! Game Over");
+                } else if (5 <= turnCount < 10) {
+                    return;
+                } else {
+                    alert("It is a tie. Play again.")
+                }
+            }
+        });
+        $('button').click(function () {
+            $('#board tr td').html("");  
+            $('#board tr td').css("background-color", "black");  
 
-header {
-    font-size: 40px;
-}
-
-table { 
-    border-collapse: collapse; 
-    border-spacing: 0; 
-}
-      
-#board { 
-    padding: 0px; 
-    margin: 0 auto; 
-    margin-top: 25px; 
-}
-      
-#board tr td { 
-    width: 80px; 
-    height: 80px; 
-    border: 1px solid white; 
-    font-family: Helvetica; 
-    font-size: 30px; 
-    text-align: center; 
-}
-
-#board tr td:hover {
-    background-color: yellow;
-}
-
-.instructions{
-    margin: 10px;
-}
-
-button {
-    background-color: blue;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-}
-
-button:hover {
-    background-color: blue;
-    color: black;
-}
-
-.bottomnav {
-    width: 100%;
-    height: 50px;
-    bottom: 100px;
-    text-align: center;
-}
-
-@media (min-height: 600px) {
-    .bottomnav {
-        position: fixed;
+        });
     }
-}
+    game();
+});
 
-.bottomnav a {
-    text-align: center;
-    font-size: 20px;
-    display: inline-block;
-    margin: 10px;
-    justify-content: space-around;
-    color: blue;
-}
 
-.bottomnav a:hover {
-    background-color: blue;
-    color: black;
-}
